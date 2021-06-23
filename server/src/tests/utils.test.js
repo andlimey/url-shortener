8265992ec4b8c7b54bcfb3cc0cbeefd26964020f
andlimey <andychan1451@gmail.com>
2021-06-23 09:09:00 +0800

    Format utils test

--- server/src/tests/utils.test.js
+++ server/src/tests/utils.test.js
@@ -2,35 +2,35 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var chai_1 = require("chai");
 var utils_1 = require("../utils");
-describe('isValidUrl()', function () {
-    context('Missing domain', function () {
-        it('Should return false', function () {
+describe("isValidUrl()", function () {
+    context("Missing domain", function () {
+        it("Should return false", function () {
             var url = "https://www.";
             chai_1.expect(utils_1.isValidUrl(url)).to.be.false;
         });
     });
-    context('Missing http/https', function () {
-        it('Should return false', function () {
+    context("Missing http/https", function () {
+        it("Should return false", function () {
             var url = "www.google.com";
             chai_1.expect(utils_1.isValidUrl(url)).to.be.false;
         });
     });
-    context('Valid Url', function () {
-        it('Should return true', function () {
+    context("Valid Url", function () {
+        it("Should return true", function () {
             var url = "https://www.google.com";
             chai_1.expect(utils_1.isValidUrl(url)).to.be.true;
         });
     });
 });
-describe('isUrlFriendly()', function () {
-    context('Contains invalid characters', function () {
-        it('Should return false', function () {
+describe("isUrlFriendly()", function () {
+    context("Contains invalid characters", function () {
+        it("Should return false", function () {
             var alias = "test|";
             chai_1.expect(utils_1.isUrlFriendly(alias)).to.be.false;
         });
     });
-    context('Valid alias', function () {
-        it('Should return true', function () {
+    context("Valid alias", function () {
+        it("Should return true", function () {
             var alias = "test";
             chai_1.expect(utils_1.isUrlFriendly(alias)).to.be.true;
         });
