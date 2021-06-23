686d21fda88ab4f83ac81da37cfadde9df657dd9
andlimey <andychan1451@gmail.com>
2021-06-23 09:11:57 +0800

    Remove redundant import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
